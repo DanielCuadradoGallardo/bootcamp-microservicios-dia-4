6295e9ae82bf1fc3b3e42e71674d1723beba6024
Daniel Cuadrado Gallardo <danielcuadradogallardo@cdpjosecabrera.es>
2023-05-11 10:01:37 +0200

    Finalizado taller 3
    Realizado el taller 3, en el que he creado un observable y dos observers suscritos al mismo, emitiendo por pantalla los mensajes del observable.

---
+++ Taller3/src/main/java/com/nttdata/taller3/App.java
@@ -0,0 +1,65 @@
+package com.nttdata.taller3;
+
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.disposables.Disposable;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) {
+        Observable<String> observable = Observable.just("Mensaje 1", "Mensaje 2", "Mensaje 3");
+
+        Observer<String> observer1 = new Observer<String>() {
+
+            @Override
+            public void onSubscribe(@NonNull Disposable disposable) {
+                System.out.println("Subscrito");
+            }
+
+            @Override
+            public void onNext(@NonNull String s) {
+                System.out.println("onNext:" + s);
+            }
+
+            @Override
+            public void onError(@NonNull Throwable throwable) {
+                System.out.println("onError: " + throwable.getMessage());
+            }
+
+            @Override
+            public void onComplete() {
+                System.out.println("onComplete");
+            }
+        };
+
+        Observer<String> observer2 = new Observer<String>() {
+            @Override
+            public void onSubscribe(@NonNull Disposable disposable) {
+                System.out.println("Subscrito");
+            }
+
+            @Override
+            public void onNext(@NonNull String s) {
+                System.out.println("onNext:" + s);
+            }
+
+            @Override
+            public void onError(@NonNull Throwable throwable) {
+                System.out.println("onError: " + throwable.getMessage());
+            }
+
+            @Override
+            public void onComplete() {
+                System.out.println("onComplete");
+            }
+        };
+
+        observable.subscribe(observer1);
+        observable.subscribe(observer2);
+    }
+}
