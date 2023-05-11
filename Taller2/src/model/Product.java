f269cd0fd426e07214828cce51c6095dc251aa2c
Daniel Cuadrado Gallardo <danielcuadradogallardo@cdpjosecabrera.es>
2023-05-11 09:16:54 +0200

    Finalizado segundo taller
    Realizada la obtención del precio de la compra mediante programación funcional, así como los productos que empiezan con la letra "C" de forma ordenada alfabéticamente.

---
+++ Taller2/src/model/Product.java
@@ -0,0 +1,17 @@
+package model;
+
+import java.math.BigDecimal;
+
+public class Product {
+        public String name;
+        public BigDecimal price;
+        public Tax tax;
+
+        public Product(String name, BigDecimal price, Tax tax) {
+            this.name = name;
+            this.price = price;
+            this.tax = tax;
+        }
+
+
+}
